de5ab95e1ebe9b042442e2aed23909b5c7478149
Elijah <100206024+EpicDuck6341@users.noreply.github.com>
2025-12-01 11:07:21 +0100

    Update ZigbeeDevice.cs

--- Elijah/Elijah.Domain/Models/ZigbeeDevice.cs
+++ Elijah/Elijah.Domain/Models/ZigbeeDevice.cs
@@ -8,18 +8,18 @@ public class ZigbeeDevice
     public string? model_id { get; set; }
     public string? description { get; set; }
 
-    // public ZigbeeDevice(
-    //     string? givenName,
-    //     string? type,
-    //     string? ieee_address,
-    //     string? model_id,
-    //     string? description
-    // )
-    // {
-    //     this.givenName = givenName ?? "";
-    //     this.ieee_address = ieee_address ?? "";
-    //     this.type = type ?? "";
-    //     this.model_id = model_id ?? "";
-    //     this.description = description ?? "";
-    // }
+    public ZigbeeDevice(
+        string? givenName,
+        string? type,
+        string? ieee_address,
+        string? model_id,
+        string? description
+    )
+    {
+        this.givenName = givenName ?? "";
+        this.ieee_address = ieee_address ?? "";
+        this.type = type ?? "";
+        this.model_id = model_id ?? "";
+        this.description = description ?? "";
+    }
 }
