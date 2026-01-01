b390bd761bcbd370ec01c06931bd6789883f7c22
Elijah <100206024+EpicDuck6341@users.noreply.github.com>
2026-01-01 15:14:19 +0100

    Update IDeviceService.cs

--- Elijah/Elijah.Logic/Abstract/IDeviceService.cs
+++ Elijah/Elijah.Logic/Abstract/IDeviceService.cs
@@ -48,4 +48,5 @@ public interface IDeviceService
     Task<List<string>> GetActiveAddressesAsync();
 
     Task SetSubscribedAsync(string address);
+    Task SetUnsubscribedAsync(string address);
 }
